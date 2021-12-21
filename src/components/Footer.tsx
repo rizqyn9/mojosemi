bd83fc55752b72422424eec4ef265015e274f39a
maulapbb <maulapbb78@gmail.com>
2021-12-21 20:28:14 +0700

    fix: bug responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,56 +7,53 @@ export function Footer() {
   return (
     <footer>
       <div className="footer-wrapper">
-        <div className="row">
-          <b>Kantor Kami</b>
-          <p>
-            Jl. Raya Sarangan Jl. Cemoro Sewu No.Km. 5, Kali Jumok, Sarangan,
-            Kec. Plaosan, Kabupaten Magetan, Jawa Timur 63361
-          </p>
-        </div>
+        <div className="container-bottom">
+          <div className="row kantor-kami">
+            <b>Kantor Kami</b>
+            <p>
+              Jl. Raya Sarangan Jl. Cemoro Sewu No.Km. 5, Kali Jumok, Sarangan,
+              Kec. Plaosan, Kabupaten Magetan, Jawa Timur 63361
+            </p>
+          </div>
 
-        <div className="row">
-          {" "}
-          <b>Kontak</b>
-          <a href="https://wa.me/628113783000" target="_blank" rel="noreel">
-            {" "}
-            <p>
-              <SiWhatsapp /> 0811-3783-000
-            </p>
-          </a>
-          <a
-            href="https://mailto:mojosemiforestpark@gmail.com"
-            target="_blank"
-            rel="noreel"
-          >
-            <p>
-              <SiMinutemailer /> mojosemiforestpark@gmail.com
-            </p>
-          </a>
-        </div>
-        <div className="row">
-          {" "}
-          <b>Media Sosial</b>
-          <a
-            href="https://www.instagram.com/mojosemiforestpark"
-            target="_blank"
-            rel="noreel"
-          >
-            {" "}
-            <p>
-              <SiInstagram /> @mojosemiforestpark
-            </p>
-          </a>
-          <a
-            href="https://www.facebook.com/mojosemidinosauruspark"
-            target="_blank"
-            rel="noreel"
-          >
-            {" "}
-            <p>
-              <SiFacebook /> Mojosemi Forest Park
-            </p>
-          </a>
+          <div className="row">
+            <b>Kontak</b>
+            <a href="https://wa.me/628113783000" target="_blank" rel="noreel">
+              <p>
+                <SiWhatsapp /> 0811-3783-000
+              </p>
+            </a>
+            <a
+              href="https://mailto:mojosemiforestpark@gmail.com"
+              target="_blank"
+              rel="noreel"
+            >
+              <p>
+                <SiMinutemailer /> mojosemiforestpark@gmail.com
+              </p>
+            </a>
+          </div>
+          <div className="row">
+            <b>Media Sosial</b>
+            <a
+              href="https://www.instagram.com/mojosemiforestpark"
+              target="_blank"
+              rel="noreel"
+            >
+              <p>
+                <SiInstagram /> @mojosemiforestpark
+              </p>
+            </a>
+            <a
+              href="https://www.facebook.com/mojosemidinosauruspark"
+              target="_blank"
+              rel="noreel"
+            >
+              <p>
+                <SiFacebook /> Mojosemi Forest Park
+              </p>
+            </a>
+          </div>
         </div>
         <div className="copyright"> Copyright Mojosemi Forest Park @2021</div>
       </div>
