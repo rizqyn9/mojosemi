0492e47c5a6d10a851765d465091247eefa34719
maulapbb <maulapbb78@gmail.com>
2021-12-22 02:46:17 +0700

    fix: footer

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,180 +1,172 @@
 import { ITicketItem } from "./pages/Infoticket";
 
 export const ticketsContent: ITicketItem[] = [
-    {
-        imgPath: "LK2.jpg",
-        title:"Dino Show",
-        date: "Sabtu, Minggu dan Hari Libur",
-        price: "15.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "atv.jpg",
-        title:"ATV",
-        date: "Setiap Hari",
-        price: "30.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "lowrope.jpg",
-        title:"Low Rope",
-        date: "Setiap Hari",
-        price: "30.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "berkuda.jpg",
-        title:"Berkuda",
-        date: "Setiap Hari",
-        price: "50.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "jltshort.png",
-        title:"Jeep Lawu Tour (Short Trip)",
-        date: "Setiap Hari",
-        price: "350.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "jltmedium.jpg",
-        title:"Jeep Lawu Tour (Medium Trip)",
-        date: "Setiap Hari",
-        price: "450.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "jltlong.jpg",
-        title:"Jeep Lawu Tour (Long Trip)",
-        date: "Setiap Hari",
-        price: "550.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Tembak Dino",
-        date: "Setiap Hari",
-        price: "10.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Basket Goyang",
-        date: "Setiap Hari",
-        price: "10.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Pukul Buaya",
-        date: "Setiap Hari",
-        price: "10.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Dart Board",
-        date: "Setiap Hari",
-        price: "10.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "archery.jpg",
-        title:"Archery",
-        date: "Setiap Hari",
-        price: "30.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "paintball.jpg",
-        title:"Paintball (Shoting Target)",
-        date: "Setiap Hari",
-        price: "30.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "paintballwg.jpg",
-        title:"Paintball (War Game)",
-        date: "Setiap Hari",
-        price: "70.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "agpendek.jpg",
-        title:"Airsoft Gun (Laras Pendek)",
-        date: "Setiap Hari",
-        price: "45.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "agpanjang.jpg",
-        title:"Airsoft Gun (Laras Panjang)",
-        date: "Setiap Hari",
-        price: "55.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "agwar.jpg",
-        title:"Airsoft Gun (War Game)",
-        date: "Setiap Hari",
-        price: "100.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "minijeep.jpg",
-        title:"Mini Jeep",
-        date: "Setiap Hari",
-        price: "30.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Mini Trail",
-        date: "Setiap Hari",
-        price: "30.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Play Ground",
-        date: "Setiap Hari",
-        price: "25.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "logo.png",
-        title:"Rolling Ball",
-        date: "Setiap Hari",
-        price: "15.000",
-        link: "",
-        desc: ""
-    },
-    {
-        imgPath: "dinoride.jpeg",
-        title:"Dino Ride",
-        date: "Setiap Hari",
-        price: "20.000",
-        link: "",
-        desc: ""
-    },
-] 
+  {
+    imgPath: "LK2.jpg",
+    title: "Dino Show",
+    date: "Sabtu, Minggu dan Hari Libur",
+    price: "15.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "atv.jpg",
+    title: "ATV",
+    date: "Setiap Hari",
+    price: "30.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "lowrope.jpg",
+    title: "Low Rope",
+    date: "Setiap Hari",
+    price: "30.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "berkuda.jpg",
+    title: "Berkuda",
+    date: "Setiap Hari",
+    price: "50.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "jltshort.png",
+    title: "Jeep Lawu Tour (Short Trip)",
+    date: "Setiap Hari",
+    price: "350.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "jltmedium.jpg",
+    title: "Jeep Lawu Tour (Medium Trip)",
+    date: "Setiap Hari",
+    price: "450.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "jltlong.jpg",
+    title: "Jeep Lawu Tour (Long Trip)",
+    date: "Setiap Hari",
+    price: "550.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "basketgoyang.jpg",
+    title: "Basket Goyang",
+    date: "Setiap Hari",
+    price: "10.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "logo.png",
+    title: "Pukul Buaya",
+    date: "Setiap Hari",
+    price: "10.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "logo.png",
+    title: "Dart Board",
+    date: "Setiap Hari",
+    price: "10.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "archery.jpg",
+    title: "Archery",
+    date: "Setiap Hari",
+    price: "30.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "paintball.jpg",
+    title: "Paintball (Shoting Target)",
+    date: "Setiap Hari",
+    price: "30.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "paintballwg.jpg",
+    title: "Paintball (War Game)",
+    date: "Setiap Hari",
+    price: "70.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "agpendek.jpg",
+    title: "Airsoft Gun (Laras Pendek)",
+    date: "Setiap Hari",
+    price: "45.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "agpanjang.jpg",
+    title: "Airsoft Gun (Laras Panjang)",
+    date: "Setiap Hari",
+    price: "55.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "agwar.jpg",
+    title: "Airsoft Gun (War Game)",
+    date: "Setiap Hari",
+    price: "100.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "minijeep.jpg",
+    title: "Mini Jeep",
+    date: "Setiap Hari",
+    price: "30.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "logo.png",
+    title: "Mini Trail",
+    date: "Setiap Hari",
+    price: "30.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "logo.png",
+    title: "Play Ground",
+    date: "Setiap Hari",
+    price: "25.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "logo.png",
+    title: "Rolling Ball",
+    date: "Setiap Hari",
+    price: "15.000",
+    link: "",
+    desc: "",
+  },
+  {
+    imgPath: "dinoride.jpeg",
+    title: "Dino Ride",
+    date: "Setiap Hari",
+    price: "20.000",
+    link: "",
+    desc: "",
+  },
+];
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
 import {
   SiWhatsapp,
   SiMinutemailer,
@@ -5,59 +7,101 @@ import {
   SiFacebook,
 } from "react-icons/si";
 
+// export function Footer() {
+//   return (
+//     <footer>
+//       <div className="footer-wrapper">
+//         <div className="container-bottom">
+//           <div className="row kantor-kami">
+//             <b>Kantor Kami</b>
+//             <p>
+//               Jl. Raya Sarangan Jl. Cemoro Sewu No.Km. 5, Kali Jumok, Sarangan,
+//               Kec. Plaosan, Kabupaten Magetan, Jawa Timur 63361
+//             </p>
+//           </div>
+
+//           <div className="row">
+//             <b>Kontak</b>
+//             <a href="https://wa.me/628113783000" target="_blank" rel="noreel">
+//               <p>
+//                 <SiWhatsapp /> 0811-3783-000
+//               </p>
+//             </a>
+//             <a
+//               href="https://mailto:mojosemiforestpark@gmail.com"
+//               target="_blank"
+//               rel="noreel"
+//             >
+//               <p>
+//                 <SiMinutemailer /> mojosemiforestpark@gmail.com
+//               </p>
+//             </a>
+//           </div>
+//           <div className="row">
+//             <b>Media Sosial</b>
+//             <a
+//               href="https://www.instagram.com/mojosemiforestpark"
+//               target="_blank"
+//               rel="noreel"
+//             >
+//               <p>
+//                 <SiInstagram /> @mojosemiforestpark
+//               </p>
+//             </a>
+//             <a
+//               href="https://www.facebook.com/mojosemidinosauruspark"
+//               target="_blank"
+//               rel="noreel"
+//             >
+//               <p>
+//                 <SiFacebook /> Mojosemi Forest Park
+//               </p>
+//             </a>
+//           </div>
+//         </div>
+//         <div className="copyright"> Copyright Mojosemi Forest Park @2021</div>
+//       </div>
+//     </footer>
+//   );
+// }
+
 export function Footer() {
+  const useLinkIcon = (
+    link: string,
+    text: string,
+    icon: ReactNode
+  ): ReactNode => {
+    return (
+      <a href={link} target="_blank" rel="noreel" className="item-container">
+        <div className="icon-container">{icon}</div>
+        <p className="content">{text}</p>
+      </a>
+    );
+  };
+
   return (
     <footer>
-      <div className="footer-wrapper">
-        <div className="container-bottom">
-          <div className="row kantor-kami">
-            <b>Kantor Kami</b>
-            <p>
-              Jl. Raya Sarangan Jl. Cemoro Sewu No.Km. 5, Kali Jumok, Sarangan,
-              Kec. Plaosan, Kabupaten Magetan, Jawa Timur 63361
-            </p>
-          </div>
-
-          <div className="row">
-            <b>Kontak</b>
-            <a href="https://wa.me/628113783000" target="_blank" rel="noreel">
+      <div className="wrapper">
+        <div className="footer-main-content">
+          <ListFooterContainer title="Kantor Kami" className="kantor-kami">
+            <div className="content">
               <p>
-                <SiWhatsapp /> 0811-3783-000
+                Jl. Raya Sarangan Jl. Cemoro Sewu No.Km. 5, Kali Jumok,
+                Sarangan,
               </p>
-            </a>
-            <a
-              href="https://mailto:mojosemiforestpark@gmail.com"
-              target="_blank"
-              rel="noreel"
-            >
-              <p>
-                <SiMinutemailer /> mojosemiforestpark@gmail.com
-              </p>
-            </a>
-          </div>
-          <div className="row">
-            <b>Media Sosial</b>
-            <a
-              href="https://www.instagram.com/mojosemiforestpark"
-              target="_blank"
-              rel="noreel"
-            >
-              <p>
-                <SiInstagram /> @mojosemiforestpark
-              </p>
-            </a>
-            <a
-              href="https://www.facebook.com/mojosemidinosauruspark"
-              target="_blank"
-              rel="noreel"
-            >
-              <p>
-                <SiFacebook /> Mojosemi Forest Park
-              </p>
-            </a>
-          </div>
+              <p>Kec. Plaosan, Kabupaten Magetan, Jawa Timur 63361</p>
+            </div>
+          </ListFooterContainer>
+          <ListFooterContainer title="Kontak">
+            {useLinkIcon("asd", "WhatsApp", <SiWhatsapp />)}
+            {useLinkIcon("asd", "WhatsApp", <SiWhatsapp />)}
+          </ListFooterContainer>
+          <ListFooterContainer title="Media Sosial">
+            {useLinkIcon("asd", "WhatsApp", <SiWhatsapp />)}
+            {useLinkIcon("asd", "WhatsApp", <SiWhatsapp />)}
+          </ListFooterContainer>
         </div>
-        <div className="copyright"> Copyright Mojosemi Forest Park @2021</div>
+        <div className="copyright">Copyright Mojosemi Forest Park @2021</div>
       </div>
     </footer>
   );
@@ -65,12 +109,15 @@ export function Footer() {
 
 interface IListFooterItem {
   title: string;
+  children?: ReactNode;
+  className?: string;
 }
 
-function ListFooterContainer() {
+function ListFooterContainer(props: IListFooterItem) {
   return (
-    <div className="footer-item-container">
-      <h2>afdfdsfsfa</h2>
+    <div className={classNames("footer-item-container", props.className)}>
+      <h2 className="title">{props.title}</h2>
+      {props.children}
     </div>
   );
 }
